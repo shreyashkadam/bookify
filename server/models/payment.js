e23ef20cc22749d6c71d689001a8263cd6f03df2
shreyashkadam <shreyash.kadam10@gmail.com>
2022-11-02 22:28:53 +0530

    Store payment details in backend

---
+++ server/models/payment.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const paymentSchema = new mongoose.Schema({
+  razorpay_order_id: {
+    type: String,
+    required: true,
+  },
+  razorpay_payment_id: {
+    type: String,
+    required: true,
+  },
+  razorpay_signature: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Payment", paymentSchema);
--- server/routes/payment.js
+++ server/routes/payment.js
@@ -1,4 +1,5 @@
 require("dotenv/config")
+const Payment = require("../models/payment");
 
 const Razorpay = require('razorpay');
 const instance = new Razorpay({
@@ -8,18 +9,42 @@ const instance = new Razorpay({
 
 const router = require("express").Router();
 
-router.post('/checkout', async(req, res) =>{
+router.post('/checkout', async (req, res) => {
+    if (!req.body.amount) {
+        return res.status(500).send({ message: "Invalid Amount" });
+    }
     const options = {
-        amount: 50000,
+        amount: Number(req.body.amount * 100),
         currency: "INR",
-      };
+    };
 
     try {
         const order = await instance.orders.create(options);
-        res.status(200).send({ order });
+        // res.status(200).send({ order });
+        res.status(200).json({
+            success: true,
+            order,
+        });
     } catch (error) {
         res.status(400).send({ success: false, msg: error });
     }
 })
+
+router.post('/paymentVerification', async (req, res) => {
+
+    const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+        req.body;
+
+    await Payment.create({
+        razorpay_order_id,
+        razorpay_payment_id,
+        razorpay_signature,
+    });
+
+    res.redirect(
+        `http://localhost:3000/paymentsuccess?reference=${razorpay_payment_id}`
+    );
+
+});
 
 module.exports = router;
