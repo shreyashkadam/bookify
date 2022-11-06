c7fcbb23636a7c269f7116b5f389f876e2f711ad
shreyashkadam <shreyash.kadam10@gmail.com>
2022-11-06 20:55:05 +0530

    Add Logo to Login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import { FcGoogle } from 'react-icons/fc'
+import {BsGoogle} from 'react-icons/bs'
 import { app } from "../config/firebase.config"
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
@@ -7,6 +8,7 @@ import { useStateValue } from '../Context/StateProvider';
 import { validateUser } from '../api';
 import { actionType } from '../Context/reducer';
 import { LoginBg } from '../assets/video';
+import { CompleteLogo } from '../assets/img/index'
 
 const Login = ({ setAuth }) => {
 
@@ -61,10 +63,11 @@ const Login = ({ setAuth }) => {
         loop
         className='w-full h-full object-cover' />
       <div className="absolute inset-0 bg-darkOverlay flex items-center justify-center p-4">
-        <div className="w-full md:w-375 p-4 bg-lightOverlay shadow-2xl rounded-md backdrop-blur-md flex flex-col items-center justify-center">
+        <div className="w-full md:w-375 p-8 bg-lightOverlay shadow-2xl rounded-md backdrop-blur-md flex flex-col items-center justify-center gap-6">
+          <img src={CompleteLogo} className="h-18"/>
           <div onClick={loginWithGoogle} className="flex items-center justify-center  gap-2 px-4 py-2 rounded-md bg-cardOverlay cursor-pointer hover:bg-card hover:shadow-md duration-100 ease-in-out transition-all"
           >
-            <FcGoogle className="text-xl" />
+            <BsGoogle className="text-xl" />
             <p>Sign in with Google</p>
           </div>
         </div>
