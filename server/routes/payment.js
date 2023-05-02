f23352ce0b4ae0475b04a4637f5ea31f6e853954
Shreyash Kadam <shreyash.kadam10@gmail.com>
2023-05-02 10:39:35 +0530

    Update server

--- server/routes/payment.js
+++ server/routes/payment.js
@@ -32,48 +32,48 @@ router.post('/checkout', async (req, res) => {
 
 router.post('/paymentVerification', async (req, res) => {
 
-    // const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
-    //     req.body;
-
-    // await Payment.create({
-    //     razorpay_order_id,
-    //     razorpay_payment_id,
-    //     razorpay_signature,
-    // });
-
-    // res.redirect(
-    //     `${clientURL}paymentsuccess?reference=${razorpay_payment_id}`
-    // );
-
     const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
         req.body;
 
-    const body = razorpay_order_id + "|" + razorpay_payment_id;
+    await Payment.create({
+        razorpay_order_id,
+        razorpay_payment_id,
+        razorpay_signature,
+    });
 
-    const expectedSignature = crypto
-        .createHmac("sha256", process.env.RAZORPAY_APT_SECRET)
-        .update(body.toString())
-        .digest("hex");
+    res.redirect(
+        `${clientURL}paymentsuccess?reference=${razorpay_payment_id}`
+    );
 
-    const isAuthentic = expectedSignature === razorpay_signature;
+    // const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+    //     req.body;
 
-    if (isAuthentic) {
-        // Database comes here
+    // const body = razorpay_order_id + "|" + razorpay_payment_id;
 
-        await Payment.create({
-            razorpay_order_id,
-            razorpay_payment_id,
-            razorpay_signature,
-        });
+    // const expectedSignature = crypto
+    //     .createHmac("sha256", process.env.RAZORPAY_APT_SECRET)
+    //     .update(body.toString())
+    //     .digest("hex");
 
-        res.redirect(
-            `${clientURL}paymentsuccess?reference=${razorpay_payment_id}`
-        );
-    } else {
-        res.status(400).json({
-            success: false,
-        });
-    }
+    // const isAuthentic = expectedSignature === razorpay_signature;
+
+    // if (isAuthentic) {
+    //     // Database comes here
+
+    //     await Payment.create({
+    //         razorpay_order_id,
+    //         razorpay_payment_id,
+    //         razorpay_signature,
+    //     });
+
+    //     res.redirect(
+    //         `${clientURL}paymentsuccess?reference=${razorpay_payment_id}`
+    //     );
+    // } else {
+    //     res.status(400).json({
+    //         success: false,
+    //     });
+    // }
 
 });
 
